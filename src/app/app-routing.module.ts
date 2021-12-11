a1baadcc978464d98e7542390a0032c3c1e5cfab
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2021-12-11 14:06:25 +0600

    Вывод списка цитат по категориям. Настройка путей роутинга. Стилизация компонентов

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { NewQuoteComponent } from './new-quote/new-quote.component';
+import { QuotesListComponent } from './quotes/quotes-list/quotes-list.component';
+
 
 const routes: Routes = [
+  {path: '', component: QuotesComponent, children: [
+      {path: '', component: QuotesListComponent},
+      {path: 'quotes/:id', component: QuotesListComponent},
+    ]},
+  {path: 'new-quote', component: NewQuoteComponent},
   {path: '**', component: NotFoundComponent},
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { NewQuoteComponent } from './new-quote/new-quote.component';
+import { QuotesListItemComponent } from './quotes/quotes-list/quotes-list-item/quotes-list-item.component';
+import { QuotesListComponent } from './quotes/quotes-list/quotes-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    QuotesComponent,
+    NewQuoteComponent,
+    QuotesListItemComponent,
+    QuotesListComponent,
   ],
   imports: [
     BrowserModule,
