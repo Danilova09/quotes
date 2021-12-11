f63dd4689c6ae209dc791a7fc2974b6e5349cdd1
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2021-12-11 14:06:10 +0600

    Вывод списка цитат по категориям. Настройка путей роутинга. Стилизация компонентов

---
+++ src/app/quotes/quotes-list/quotes-list-item/quotes-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quotes-list-item',
+  templateUrl: './quotes-list-item.component.html',
+  styleUrls: ['./quotes-list-item.component.css']
+})
+export class QuotesListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
